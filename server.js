6d8dd82511e2894a58054fae5b598b778e8c6785
Sean Valdivia <irishseanvaldivia@gmail.com>
2018-05-29 09:03:03 -0500

    Starting on routes now...

--- server.js
+++ server.js
@@ -3,7 +3,14 @@ const express = require('express');
 const server = express();
 const port = 8000;
 
+server.use(express.json());
+
 server.get('/', (req, res) => {
     res.send(`=== SERVER RUNNING ON PORT ${port} ===`);
     console.log("It's working!");
+});
+
+
+server.listen(port, () => {
+    console.log(` === API IS RUNNING ON PORT ${port} === `)
 })
